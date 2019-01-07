4afc86aafffc3dfa4653ba8df9c97923d96be3c6
Daniel Schuster <daniel.schuster@ima-zlw-ifu.rwth-aachen.de>
2019-01-07 17:53:10 +0100

    added docstrings

--- pm4py/algo/conformance/alignments/most_probable_alignments/utils.py
+++ pm4py/algo/conformance/alignments/most_probable_alignments/utils.py
@@ -3,6 +3,12 @@ from pm4py.algo.conformance.alignments.utils import SKIP  # skip symbol, usual: 
 
 
 def calculate_log_move_probability(event_log, prior=None):
+    """
+    Given an event_log and an optional prior the method calculates the log move probabilities for each unique event
+    :param event_log: <class 'pm4py.objects.log.log.TraceLog'>
+    :param prior: <class 'dict'>, e.g.: {'A': 1, 'B': 1, 'C': 1, 'D': 1, '*': 1}
+    :return: <class 'dict'>, e.g.: {'A': 0.18795620437956204, 'B': 0.12439172749391728, ...}
+    """
     event_frequencies_log = _count_event_frequencies(event_log)
     event_frequencies = _sum_up_prior_and_log_event_frequencies(event_frequencies_log, prior)
     total_number_event_names = len(event_frequencies)
@@ -17,6 +23,11 @@ def calculate_log_move_probability(event_log, prior=None):
 
 
 def _count_event_frequencies(event_log):
+    """
+    Takes a log and counts the occurrence of each unique event
+    :param event_log: <class 'pm4py.objects.log.log.TraceLog'>
+    :return: <class 'dict'>, e.g.: {'A': 203, 'B': 412, 'C': 162, 'D': 40}
+    """
     event_frequencies = {}
     for trace in event_log:
         for event in trace.get_event_list():
@@ -29,6 +40,11 @@ def _count_event_frequencies(event_log):
 
 
 def _sum_up_prior_and_log_event_frequencies(event_frequencies_log, event_frequencies_prior):
+    """
+    :param event_frequencies_log: <class 'dict'>, e.g.: {'A': 203, 'B': 412, 'C': 162, 'D': 40}
+    :param event_frequencies_prior: <class 'dict'>, e.g.: {'A': 1, 'B': 1, 'C': 1, 'D': 1, '*': 1}
+    :return: <class 'dict'>, e.g.: {'A': 204, 'B': 413, 'C': 163, 'D': 41, '*': 1}
+    """
     res = {}
     if event_frequencies_prior:
         for event in event_frequencies_log:
@@ -43,6 +59,18 @@ def _sum_up_prior_and_log_event_frequencies(event_frequencies_log, event_frequen
 
 
 def calculate_model_move_probabilities_without_prior(event_log, petri_net, initial_marking, final_marking):
+    """
+    Function calculates the model move probabilities for a petri net (with initial/final marking) and an event log
+    :param event_log: <class 'pm4py.objects.log.log.TraceLog'>
+    :param petri_net: <class 'pm4py.objects.petri.petrinet.PetriNet'>
+    :param initial_marking: <class 'pm4py.objects.petri.petrinet.Marking'>
+    :param final_marking: <class 'pm4py.objects.petri.petrinet.Marking'>
+    :return: <class 'list'>, e.g.:
+    [{'marking': {'p_1': 1}, 'outgoing_transitions':
+                                            [{'unique_name': 't_A', 'frequency': 203, 'model_move_probability': 1.0}]
+     },...]
+    """
+
     # if parameters are none, the standard cost function will be applied ->
     #   cost 1000   log/model moves
     #   cost 1      tau moves
@@ -63,14 +91,6 @@ def calculate_model_move_probabilities_without_prior(event_log, petri_net, initi
                         # only consider process net places
                         number_tokens = move['marking_before_transition'][place]
                         current_marking[place.name[1]] = number_tokens
-
-                # log_move_probabilities_given_marking is a list of dict like below:
-                # {
-                #     "marking": current_marking,
-                #     "outgoing_transitions": [
-                #         {"unique_name": move['name'][1], "frequency": xx, "model_move_probability": xx}
-                #     ]
-                # }
 
                 founded_dict = next(
                     (x for x in log_move_probabilities_given_marking if x['marking'] == current_marking), None)
@@ -104,6 +124,15 @@ def calculate_model_move_probabilities_without_prior(event_log, petri_net, initi
 
 
 def print_most_probable_alignment(alignment):
+    """
+    Takes an alignment and prints the alignment to the console, e.g.:
+     A  | B  | C  | D  |
+    --------------------
+     A  | B  | C  | >> |
+
+    :param alignment: <class 'dict'>
+    :return: Nothing
+    """
     print("\nprobability: ", alignment['probability'], ' ~%.2f' % (alignment['probability'] * 100), '%')
 
     trace_steps = []
