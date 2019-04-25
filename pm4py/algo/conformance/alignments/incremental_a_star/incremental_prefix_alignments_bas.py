bd429b7be5863c05a43682b25e7b1031262f7221
Daniel Schuster <daniel.schuster@ima-zlw-ifu.rwth-aachen.de>
2019-04-25 10:25:08 +0200

    fixed bugs in incremental prefix alignments bas

--- pm4py/algo/conformance/alignments/incremental_a_star/incremental_prefix_alignments_bas.py
+++ pm4py/algo/conformance/alignments/incremental_a_star/incremental_prefix_alignments_bas.py
@@ -12,8 +12,10 @@ from pm4py.objects.log.log import Trace
 from pm4py.util.constants import PARAMETER_CONSTANT_ACTIVITY_KEY
 from pm4py.algo.conformance.alignments.utils import SKIP, print_alignment
 from pm4py.visualization.petrinet import factory as pn_vis_factory
-from pm4py.algo.conformance.alignments.most_probable_alignments.miscellaneous_utils import is_log_move
+from pm4py.algo.conformance.alignments.most_probable_alignments.miscellaneous_utils import is_log_move, \
+    place_from_synchronous_product_net_belongs_to_trace_net_part
 from pm4py.algo.conformance.alignments.versions.state_equation_a_star import SearchTuple
+from pm4py.objects.petri.semantics import enabled_transitions
 
 
 def apply(trace, petri_net, initial_marking, final_marking, parameters=None, debug_print=True, window_size=0):
@@ -33,11 +35,18 @@ def apply(trace, petri_net, initial_marking, final_marking, parameters=None, deb
     intermediate_results = []
     heuristic_computation_time_total = 0
 
+    current_marking = None
+
     for event in trace:
         incremental_trace.append(event)
+
         if debug_print:
-            print(incremental_trace)
-            print("event", len(incremental_trace), "/", len(trace))
+            trace_as_string = ""
+            for e in incremental_trace:
+                trace_as_string += "," + e[activity_key]
+            print(trace_as_string)
+            print("event", len(incremental_trace), "/", len(trace), "\n")
+
         if first_event:
             # activity_key: :class:`str` key of the attribute of the events that defines the activity name
             trace_net, trace_im, trace_fm = petri.utils.construct_trace_net(incremental_trace,
@@ -45,6 +54,7 @@ def apply(trace, petri_net, initial_marking, final_marking, parameters=None, deb
             sync_prod, sync_im, sync_fm = petri.synchronous_product.construct(trace_net, trace_im, trace_fm, petri_net,
                                                                               initial_marking, final_marking, SKIP)
             first_event = False
+            current_marking = sync_im
         else:
             sync_prod, sync_fm = petri.synchronous_product.extend_trace_net_of_synchronous_product_net(sync_prod, event,
                                                                                                        sync_fm, SKIP,
@@ -56,9 +66,13 @@ def apply(trace, petri_net, initial_marking, final_marking, parameters=None, deb
             # pn_vis_factory.view(gviz)
 
         cost_function = alignments.utils.construct_standard_cost_function(sync_prod, SKIP)
-        res = __calculate_prefix_alignment_for_next_event(petri_net, sync_prod, sync_im, sync_fm, cost_function,
-                                                          SKIP, prefix_alignment, trace, activity_key, window_size)
-
+        if not current_marking:
+            current_marking = sync_im
+        res = __calculate_prefix_alignment_for_next_event(petri_net, sync_prod, sync_im, sync_fm, current_marking,
+                                                          cost_function,
+                                                          SKIP, prefix_alignment, incremental_trace, activity_key,
+                                                          window_size)
+        prefix_alignment = res['alignment']
         # update statistic values
         visited_states_total += res['visited_states']
         traversed_arcs_total += res['traversed_arcs']
@@ -74,9 +88,11 @@ def apply(trace, petri_net, initial_marking, final_marking, parameters=None, deb
                             'total_computation_time': time.time() - start_time,
                             'heuristic_computation_time': heuristic_computation_time_total}
         intermediate_results.append(intermediate_res)
-
+        current_marking = res['alignment'][-1]['marking_after_transition']
         if debug_print:
             print_alignment(res)
+            print("\n Marking:")
+            print(current_marking)
 
     duration_total = time.time() - start_time
     return {'alignment': res['alignment'], 'cost': res['cost'],
@@ -86,9 +102,9 @@ def apply(trace, petri_net, initial_marking, final_marking, parameters=None, deb
             'intermediate_results': intermediate_results}
 
 
-def __calculate_prefix_alignment_for_next_event(process_net, sync_net, start_marking, final_marking, cost_function,
-                                                skip,
-                                                prefix_alignment, trace, activity_key, window_size, debug_print=True):
+def __calculate_prefix_alignment_for_next_event(process_net, sync_net, initial_marking, final_marking,
+                                                marking_after_prefix_alignment, cost_function, skip, prefix_alignment,
+                                                trace, activity_key, window_size, debug_print=True):
     start_time = time.time()
     event_to_align = trace[len(trace) - 1]
 
@@ -109,35 +125,32 @@ def __calculate_prefix_alignment_for_next_event(process_net, sync_net, start_mar
         if t.label == activity_name:
             # there is a corresponding transition in the process net
 
-            synchronous_move_transition_enabled = None
             synchronous_move_transition = None
-
             for t_s in sync_net.transitions:
-                if t_s.label[0] == t_s.label[1] == activity_name:
-                    # there is a corresponding synchronous move transition in the synchronous product net
+                if t_s.label[0] == t_s.label[1] == activity_name and \
+                        t_s in enabled_transitions(sync_net, marking_after_prefix_alignment):
+                    # there is a corresponding activated synchronous move transition in the synchronous product net
                     synchronous_move_transition = t_s
-                    synchronous_move_transition_enabled = t_s in petri.semantics.enabled_transitions(sync_net,
-                                                                                                     start_marking)
-                    # since there can be only one synchronous move transition for the given activity we can break here
                     break
 
-            if synchronous_move_transition_enabled and synchronous_move_transition:
+            if synchronous_move_transition:
                 # ADD SYNCHRONOUS MOVE
                 if debug_print:
                     print("ADD SYNCHRONOUS MOVE ")
-                new_marking = petri.semantics.execute(synchronous_move_transition, sync_net, start_marking)
+                new_marking = petri.semantics.execute(synchronous_move_transition, sync_net,
+                                                      marking_after_prefix_alignment)
 
-                cost_of_synchronous_move = cost_function(synchronous_move_transition)
+                cost_of_synchronous_move = cost_function[synchronous_move_transition]
                 if len(prefix_alignment) > 0:
                     cost_prefix_alignment = cost_so_far + cost_of_synchronous_move
                 else:
                     # first step in alignment
                     cost_prefix_alignment = cost_of_synchronous_move
                     # add sync move to alignment
-                prefix_alignment = prefix_alignment + [{"marking_before_transition": start_marking,
+                prefix_alignment = prefix_alignment + [{"marking_before_transition": initial_marking,
                                                         "label": synchronous_move_transition.label,
                                                         "name": synchronous_move_transition.name,
-                                                        "cost_so_far": cost_function(synchronous_move_transition),
+                                                        "cost_so_far": cost_function[synchronous_move_transition],
                                                         "marking_after_transition": new_marking}]
                 return {'alignment': prefix_alignment,
                         'cost': cost_prefix_alignment,
@@ -150,7 +163,7 @@ def __calculate_prefix_alignment_for_next_event(process_net, sync_net, start_mar
                 # USE A* TO FIND NEW OPTIMAL ALIGNMENT
                 if debug_print:
                     print("START FROM SCRATCH -> A*")
-                res = __search(sync_net, start_marking, final_marking, cost_function, skip, cost_so_far)
+                res = __search(sync_net, initial_marking, final_marking, cost_function, skip, cost_so_far)
                 return {'alignment': res['alignment'],
                         'cost': res['cost'] + cost_so_far,
                         'visited_states': res['visited_states'],
@@ -162,22 +175,23 @@ def __calculate_prefix_alignment_for_next_event(process_net, sync_net, start_mar
     if debug_print:
         print("ADD LOG MOVE")
     for t in sync_net.transitions:
-        if is_log_move(t) and t.label[0] == activity_name and petri.semantics.is_enabled(t, sync_net, start_marking):
-            new_marking = petri.semantics.execute(t, sync_net, start_marking)
-            prefix_alignment = prefix_alignment + [{"marking_before_transition": start_marking,
+        if is_log_move(t, skip) and t.label[0] == activity_name and petri.semantics.is_enabled(t, sync_net,
+                                                                                               marking_after_prefix_alignment):
+            new_marking = petri.semantics.execute(t, sync_net, marking_after_prefix_alignment)
+            # TODO get rid off static cost for log move
+            prefix_alignment = prefix_alignment + [{"marking_before_transition": initial_marking,
                                                     "label": t.label,
                                                     "name": t.name,
-                                                    "cost_so_far": cost_function(t) + cost_so_far,
+                                                    "cost_so_far": 1000 + cost_so_far,
                                                     "marking_after_transition": new_marking}]
             return {'alignment': prefix_alignment,
-                    'cost': cost_function(t) + cost_so_far,
+                    'cost': 1000 + cost_so_far,
                     'visited_states': 0,
                     'queued_states': 0, 'traversed_arcs': 0,
                     'total_computation_time': time.time() - start_time,
                     'heuristic_computation_time': 0}
-        else:
-            # this code should be never reached if everything is okay
-            raise Exception('No corresponding log move transition found in sync net')
+
+    raise Exception('No corresponding log move transition found in sync net')
 
 
 def __search(sync_net, start_marking, final_marking, cost_function, skip, cost_to_reach_start_marking):
@@ -213,9 +227,16 @@ def __search(sync_net, start_marking, final_marking, cost_function, skip, cost_t
         visited += 1
         current_marking = curr.m
         closed_set.add(current_marking)
-        if current_marking == final_marking:
-            return __reconstruct_alignment(curr, visited, queued, traversed, time.time() - start_time,
-                                           heuristic_time)
+
+        # check if we reached the final marking in the trace net part
+        for place in current_marking:
+            if place_from_synchronous_product_net_belongs_to_trace_net_part(place):
+                for place2 in final_marking:
+                    if place_from_synchronous_product_net_belongs_to_trace_net_part(place2):
+                        if place.name == place2.name:
+                            # found a final marking of the trace net
+                            return __reconstruct_alignment(curr, visited, queued, traversed, time.time() - start_time,
+                                                           heuristic_time)
 
         for t in petri.semantics.enabled_transitions(sync_net, current_marking):
             if curr.t is not None and __is_log_move(curr.t, skip) and __is_model_move(t, skip):
